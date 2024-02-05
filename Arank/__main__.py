d43b4f794a896717aa7b8c5b0a99c0615a4062e9
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2024-02-05 05:43:14 -0800

    Update __main__.py

--- Arank/__main__.py
+++ Arank/__main__.py
@@ -17,7 +17,7 @@ async def anony_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("Arank.modules." + all_module)
 
-    LOGGER.info(f"@{Itsz_Kanishka_Babess_Bot.username} Started.")
+    LOGGER.info(f"@{Kanishka.username} Started.")
     await idle()
 
 
